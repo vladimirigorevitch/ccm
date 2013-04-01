0fd6541f59fc10c644af3f2248c9e98690bd3b28
robosovyka <robosovyaka@gmail.com>
2013-04-02 00:50:06 +0700

    getChar implementation

--- core_src/core_FilePointer.cpp
+++ core_src/core_FilePointer.cpp
@@ -4,16 +4,31 @@ namespace core
 {
 namespace file_pointer
 {
-	FilePointer::FilePointer(const char * _fl_name)
+	FilePointer::FilePointer(const char * _fl_name) : fl(_fl_name)
 	{
-		std::FILE *fl = std::fopen(_fl_name, "r");
 		file_size = getFileSize();
 	};
 	FilePointer::~FilePointer()
 	{
-		std::fclose(fl);
+		fl.close();
 
 	}
+	uint32_t FilePointer::getFileSize()
+	{
+		long end, begin;
+		begin = fl.tellg();
+		fl.seekg(0, std::ios::end);
+		end = fl.tellg();
+		return (end-begin);
+	}
 
+	char FilePointer::getChar(uint8_t offset)
+	{
+		char retChar;
+		fl.seekg(static_cast<std::streampos>(offset));
+		retChar = fl.get();
+		if(fl.good()) return retChar;
+		else throw EoFErr();
+	}
 }
 }
