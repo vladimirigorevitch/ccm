e77f55fd919d19cc4e49b132cfb7940ab06d91fa
Sergey <gh0st3103@gmail.com>
2013-04-10 23:28:56 +0700

    Cosmetic changes
    lol

--- core_src/Parser.cpp
+++ core_src/Parser.cpp
@@ -2,8 +2,64 @@
 
 namespace sintax
 {
+	types::Token Parser::takeToken ()
+	{
+		//add to the id table
+	}
 
-	/*token gettoken () const;
-	void addoperation ();*/
+	void Parser::parse ()
+	{
+		while ( tokensStillRemain() )
+		{
+			types::Token new_token = takeToken();
+			switch (new_token.getTokenType())              //TODO: Thinking to break into private functions
+			{
+			case types::VARIABLE :
+				{
+				
+					if ( false == new_token.isLvalue() )
+					{
+						//TODO: error
+						break;
+					}
+					tokens.push_back(new_token);
+					new_token = takeToken();
 
+					if ( ASSIGNMENT == new_token.getTokenName())
+					{
+						//build function for adding arithmetics w/ RPN 
+						//addOperation(new_token , tokens.pop_back() ,  )
+						/*while ( semicolon != (new_token = taketoken()).gettokenname() )
+						{
+							
+						}*/
+					}
+					else if ( INCREMENT == new_token.getTokenName() )
+					{
+						//create constant token "1" add operation
+					}
+					else if ( DECREMENT == new_token.getTokenName() )
+					{
+						//create constant token "1" add operation
+					}
+
+					
+				}
+			case types::KEY_WORD :
+				{
+					//TODO: for/while/do_while/if
+				}
+			case types::FUNCTION :
+				{
+					//TODO: func add body
+				}
+			default:
+				{
+					//TODO: error
+					break;
+				}
+			}
+			//tokens.push_back();
+		}
+	}
 }
--- core_src/headers/Parser.h
+++ core_src/headers/Parser.h
@@ -4,8 +4,11 @@
 
 namespace sintax
 {
-	const char* LVALUE = "lvalue";
-	const char* RVALUE = "rvalue";
+
+	const char* INCREMENT = "++";
+	const char* DECREMENT = "--";
+	const char* ASSIGNMENT = "=";
+	const char* SEMICOLON = ";";
 
 	class Parser 
 	{
@@ -13,13 +16,16 @@ namespace sintax
 			Parser ();
 			~Parser();
 
+			void parse ();
+			bool tokensStillRemain () const;
+
+
 		private :
 			types::Tree tree;
 			std::vector< types::Token > tokens; 
 
 			types::Token takeToken ();
-			void addOperation ();
-			
+			void addOperation ( types::Token operation , types::Token operand_fst , types::Token operand_scnd );
 
 
 	};
